db19434f2dc74b0ca5ddbba036ca9b31069db7dd
lordofsaca <lukoualive@163.com>
2024-04-22 01:46:05 +0800

    save

--- frontend/triangle_chess/src/views/Login/index.vue
+++ frontend/triangle_chess/src/views/Login/index.vue
@@ -32,31 +32,7 @@ const login = async () => {
   }
 };
 
-const register = async() =>{
-  try{
 
-      const params = new URLSearchParams();
-      params.append('uname', uname.value);
-      params.append('password', password.value);
-      const paramsString = 'http://localhost:9002/user/register?' + params.toString();
-      const response = await fetch(paramsString, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          uname: uname.value,
-          password: password.value
-        })
-      });
-    if (!response.ok) {
-      throw new Error('注册失败');
-    }
-  }
-  catch (error) {
-    errorMessage.value = '网络异常，请稍后再试';
-  }
-}
 </script>
 
 <template>
---
+++ frontend/triangle_chess/src/views/Register/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const uname = ref('');
+const password = ref('');
+const errorMessage = ref('');
+
+const register = async() =>{
+    try{
+
+        const params = new URLSearchParams();
+        params.append('uname', uname.value);
+        params.append('password', password.value);
+        const paramsString = 'http://localhost:9002/user/register?' + params.toString();
+        const response = await fetch(paramsString, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        uname: uname.value,
+        password: password.value
+        })
+    });
+    if (!response.ok) {
+    throw new Error('注册失败');
+    }
+}
+catch (error) {
+    errorMessage.value = '网络异常，请稍后再试';
+}
+}
+</script>
+
+<template>
+    
+</template>
